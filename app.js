4bdb0ff5affd90e0f25b8dcc47903ca7847f093e
Liudan <396183998@qq.com>
2018-11-24 22:15:16 +0800

    test

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//app.js
+//app.js 
 App({
   onLaunch: function () {
     // 展示本地存储能力
@@ -7,14 +7,14 @@ App({
     wx.setStorageSync('logs', logs)
 
     // 登录
-    wx.login({
+    wx.login({ 
       success: res => {
         console.log(res)
         console.log(66688888)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
-    // 获取用户信息
+    // 获取用户信息 
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
@@ -23,16 +23,6 @@ App({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
-              console.log(res)
-              console.log('+++++++++++++')
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                console.log(this.userInfoReadyCallback)
-                console.log(999999)
-                this.userInfoReadyCallback(res)
-              }
             }
           })
         }
