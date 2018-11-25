c0672214a835a6f4dcea36f9c2e4765839f1c497
朝鲜咯 <suns2t@163.com>
2018-11-25 23:35:02 +0800

    homePageTempSave

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//app.js 
+//app.js
 App({
   onLaunch: function () {
     // 展示本地存储能力
@@ -7,12 +7,12 @@ App({
     wx.setStorageSync('logs', logs)
 
     // 登录
-    wx.login({ 
+    wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
-    // 获取用户信息 
+    // 获取用户信息
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
@@ -21,6 +21,12 @@ App({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
             }
           })
         }
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,17 +4,71 @@ const app = getApp()
 
 Page({
   data: {
-    userInfo: {},
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  onLoad: function () {
+    imgUrls: [
+      '../../images/banner.png',
+      '../../images/banner.png',
+      '../../images/banner.png',
+      '../../images/banner.png'
+    ],
+    currentDot: 0,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    text: '陪护床收纳在床边柜里，自愿付费使用。租借后，打开柜门，拉出陪护床。归还时，将陪护床推进床边柜，自动上锁。',
+    marqueePace: 1,//滚动速度
+    marqueeDistance: 82,//初始滚动距离
+    marqueecopy_status: false,
+    marquee_margin: 10,
+    size: 12,
+    orientation: 'left',//滚动方向
+    timeInterval: 20 // 时间间隔
 
   },
-  getUserInfo: function(e) {
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+ 
+  onLoad: function () {
+   
+  },
+  swiperChange: function (e) {
+    this.setData({ currentDot: e.detail.current})
+  },
+  onShow: function () {
+    // 页面显示
+    var vm = this;
+    var length = vm.data.text.length * vm.data.size;//文字长度
+    var windowWidth = wx.getSystemInfoSync().windowWidth;// 屏幕宽度
+    vm.setData({
+      length: length,
+      windowWidth: windowWidth,
+      marquee_margin: length < windowWidth ? windowWidth - length : vm.data.marquee_margin//当文字长度小于屏幕长度时，需要增加补白
+    });
+    vm.run2();// 第一个字消失后立即从右边出现
+  },
+
+  run2: function () {
+    var vm = this;
+    var interval = setInterval(function () {
+      if (-vm.data.marqueeDistance < vm.data.length) {
+        // 如果文字滚动到出现marquee_margin=30px的白边，就接着显示
+        vm.setData({
+          marqueeDistance: vm.data.marqueeDistance - vm.data.marqueePace,
+          marqueecopy_status: vm.data.length + vm.data.marqueeDistance <= vm.data.windowWidth + vm.data.marquee_margin,
+        });
+      } else {
+       if (-vm.data.marqueeDistance >= vm.data.marquee_margin) { // 当第二条文字滚动到最左边时
+          clearInterval(interval);
+          vm.setData({
+            marqueeDistance: vm.data.marquee_margin // 直接重新滚动
+          });
+         
+          vm.run2();
+        } else {
+          clearInterval(interval);
+          vm.setData({
+            marqueeDistance: -vm.data.windowWidth
+          });
+          vm.run2();
+        }
+      }
+    }, vm.data.timeInterval);
   }
 })
