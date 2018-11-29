e92bbbb28bb4901c854fa780c54e2a7d547bfe68
Liudan <396183998@qq.com>
2018-11-29 23:56:54 +0800

    new pages

---
+++ pages/withdrawCash/withdrawCash.js
@@ -0,0 +1,79 @@
+// pages/withdrawCash/withdrawCash.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    money:8888,
+    money_value:''
+  },
+  getAllMoney:function(e){
+    var money_value = e.currentTarget.dataset.money;
+    this.setData({
+      money_value
+    })
+  },
+  getMoney:function(e){
+    var money_value = e.detail.value;
+    this.setData({
+      money_value
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
